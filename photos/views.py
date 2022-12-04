b7942490a4e83872da2f7db754a99bf58bacb4cf
MohsenSami <mohsensami88@gmail.com>
2022-12-04 17:14:05 +0330

    show simple gallery and single photo

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-
+from .models import Category, Photo
 
 
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+    photos = Photo.objects.all()
+    categories = Category.objects.all()
+    context = {'categories': categories, 'photos': photos}
+    return render(request, 'photos/gallery.html', context)
+
+def viewPhoto(request, pk):
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo': photo})
 
 def addPhoto(request):
     return render(request, 'photos/add.html')
 
-def viewPhoto(request, pk):
-    return render(request, 'photos/photo.html')

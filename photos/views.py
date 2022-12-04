db062f305a02ce913f1d7ee7ee8f8ae33de6787b
MohsenSami <mohsensami88@gmail.com>
2022-12-04 16:18:04 +0330

    add photos app and add urls temlates and sample view

---
+++ photos/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+
+def gallery(request):
+    return render(request, 'photos/gallery.html')
+
+def viewPhoto(request):
+    return render(request, 'photos/add.html')
+
+def addPhoto(request):
+    return render(request, 'photos/photo.html')

eec197f6c6188a920fc2ee80ced5ecf8180933b6
MohsenSami <mohsensami88@gmail.com>
2022-12-04 18:18:44 +0330

    add authentication and template for login and register and add all view.py for multi add photo and seperate base.html for extends.

--- photos/models.py
+++ photos/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=100, null=False, blank=False)
 
     class Meta:
